78005056287d8481ef6a1ded2125176714aa4d69
Xukeyu <148688622+xukeyu-phy@users.noreply.github.com>
2026-09-17 15:23:12 +0800

    updated FOMmain

--- DME_main.py
+++ DME_main.py
@@ -13,21 +13,19 @@ outdata_dir.mkdir(exist_ok=True)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float64
 torch.set_default_dtype(dtype)
-
+grid_type = 'uniform'  # Optional: 'uniform' or 'ratio'
 
 def main():
     param_filename = current_dir / 'parameters.json'
     with open(param_filename, 'r') as f:
         phy_dict = json.load(f)
-    grid_type = 'uniform'  # Optional: 'uniform' or 'ratio'
-    xi_method = 'EI'    # Optional: 'approx', 'init', 'RE-BDF2', 'BE-BDF2', 'CN', 'EI'
-    config = Config(device, dtype)
+        config = Config(device, dtype)
     
     if grid_type == 'ratio':    
         config._create_equal_ratio_grid(outdata_dir, phy_dict)
     else:
         config._create_non_uniform_grid(outdata_dir)
-    dmesolver = DMESolver(device, dtype, phy_dict, grid_type, xi_method, outdata_dir)
+    dmesolver = DMESolver(device, dtype, phy_dict, grid_type, outdata_dir)
     dmesolver._init_phy_ps(phy_dict)
     dt = config._get_time_step(phy_dict)
 
@@ -38,5 +36,4 @@ def main():
 
 
 if __name__ == "__main__":
-
     main()
